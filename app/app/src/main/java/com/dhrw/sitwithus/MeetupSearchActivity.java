2e1de82661b339b77be4babaac751205232f2e35
wmcrain <wmcrain@crimson.ua.edu>
2017-12-07 12:58:21 -0600

    Meetup Match dialogue color visible

--- app/app/src/main/java/com/dhrw/sitwithus/MeetupSearchActivity.java
+++ app/app/src/main/java/com/dhrw/sitwithus/MeetupSearchActivity.java
@@ -18,6 +18,7 @@ import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -174,8 +175,8 @@ public class MeetupSearchActivity extends Activity {
 
             //
             TextView meetupName = (TextView) view.findViewById(R.id.confirm_match_text);
-            meetupName.setTextColor(Color.parseColor("#ffffff"));
-            meetupName.setBackgroundColor(Color.parseColor("#000000"));
+            meetupName.setGravity(Gravity.CENTER);
+            meetupName.setTextColor(Color.parseColor("#000000"));
             meetupName.setText(firstName + " " + lastName + " wants to match with you!");
 
             // The the image of the person this user matched with
